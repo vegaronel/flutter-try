725f63a3bb371d7065f597bf18f5704e2fd283fa
ronelvega19 <ronelvega31@gmail.com>
2024-06-16 19:25:28 +0800

    try layout

--- lib/home.dart
+++ lib/home.dart
@@ -12,39 +12,29 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    int num1 = 1;
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           "Hello Flutter",
-          style: TextStyle(fontSize: 20, color: Colors.black),
+          style: TextStyle(fontSize: 20, color: Colors.white),
         ),
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.amberAccent,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text("Centers 1"),
-            const Text("Center 2"),
-            const Text("Center 3"),
+           const Text("Cat"), 
+           const Text("Dog"), 
+           const Text("Bird"),
             Container(
               color: Colors.amber,
-              child: TextButton(
-                onPressed: () => {
-                  num1++,
-                  print("Hello Flutter" + num1.toString()),
-                },
-                child: const Text(
-                  "Text Button",
-                  style: TextStyle(color: Colors.white),
-                ),
-                style: ButtonStyle(
-                    backgroundColor:
-                        WidgetStateProperty.all<Color>(Colors.blue)),
-              ),
-            )
-          ],
+              child: TextButton(onPressed: () => {
+                print("Hello Flutter"),
+              }, child: Text("Click here", style: TextStyle(color: Colors.black),),),
+              )
+            ],
+          
         ),
       ),
     );
